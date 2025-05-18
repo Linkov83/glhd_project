cd72b522aabfd9477dc621809400d3c0fef924ae
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-18 13:25:29 +0300

    new model

--- database/models.py
+++ database/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
 class Experiment(models.Model):
-    metal_class = models.CharField(max_length=100)
-    subclass = models.CharField(max_length=100)
-    example_alloy = models.CharField(max_length=100)
-    chemical_composition = models.TextField()
-    laser_power_w = models.FloatField()
-    scanning_speed_mm_s = models.FloatField()
-    beam_spot_size_and_profile = models.CharField(max_length=100)
-    beam_quality_m2 = models.CharField(max_length=100)
-    surface_material = models.CharField(max_length=100)
-    pre_treatment = models.CharField(max_length=100)
-    temperature_range_c = models.CharField(max_length=100)
-    surface_hardness_hv = models.FloatField()
-    hardened_layer_depth_mm = models.FloatField()
-    residual_stresses = models.CharField(max_length=100)
-    wear_resistance = models.CharField(max_length=100)
-    source = models.CharField(max_length=255)
+    Metal_Class = models.CharField(max_length=100)
+    Subclass = models.CharField(max_length=100)
+    Example_Alloy = models.CharField(max_length=100)
+    Chemical_Composition = models.TextField()
+    Laser_Power_W = models.FloatField()
+    Scanning_Speed_mm_s = models.FloatField()
+    Beam_Spot_Size_and_Profile = models.CharField(max_length=100)
+    Beam_Quality_M2 = models.CharField(max_length=100)
+    Surface_Material = models.CharField(max_length=100)
+    Pre_Treatment = models.CharField(max_length=100)
+    Temperature_Range_C = models.CharField(max_length=50)
+    Surface_Hardness_HV = models.FloatField()
+    Hardened_Layer_Depth_mm = models.FloatField()
+    Residual_Stresses = models.CharField(max_length=100)
+    Wear_Resistance = models.CharField(max_length=100)
+    Source = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.example_alloy} – {self.laser_power_w}W"
+        return f"{self.Example_Alloy} – {self.Laser_Power_W}W @ {self.Scanning_Speed_mm_s}mm/s
