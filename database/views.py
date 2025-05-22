5960158c5577834068ddbd127ff175cd9073737f
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-23 00:56:22 +0300

    Local changes before pulling remote

--- database/views.py
+++ database/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-
 from .models import Experiment
 from .serializers import ExperimentSerializer
 
