02de83a69e1c16628a2e309c74619b31b28c65bd
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-23 01:20:16 +0300

    Resolved conflicts

--- database/serializers.py
+++ database/serializers.py
@@ -5,5 +5,5 @@ from .models import Experiment
 class ExperimentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experiment
-        fields = '_all_'
+        fields = "__all__"
         read_only_fields = ['approved']
