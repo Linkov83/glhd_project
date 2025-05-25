50331b2862b06e08ccf006ffaa4d890ba45e2789
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-25 15:11:37 +0300

    fixes for Render

--- glhd_project/settings.py
+++ glhd_project/settings.py
@@ -105,10 +105,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
