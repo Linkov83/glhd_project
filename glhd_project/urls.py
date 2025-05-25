ed6c1a871b51accfabdfe0d266a22615c7a04f24
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-25 12:31:12 +0300

    add djoser

--- glhd_project/urls.py
+++ glhd_project/urls.py
@@ -8,5 +8,10 @@ def index(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/experiments/', include('database.urls')),
+
+    path('auth/', include('djoser.urls')),
+
+    path('auth/', include('djoser.urls.jwt')),
     path('', index),
+
 ]
