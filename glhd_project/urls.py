3c100bb29e6b6431ebeafcf80a82532e73421534
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-25 18:21:00 +0300

    fix login

--- glhd_project/urls.py
+++ glhd_project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
     path('api/experiments/', include('database.urls')),
+
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
