c5ca0897d1633dc52f3b8d108936c7c1aba3c8ef
Lyubomir Linkov <kubratovec@gmail.com>
2025-05-28 19:04:04 +0300

    Fix experiments

--- database/serializers.py
+++ database/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 from .models import Experiment
 from django.contrib.auth import get_user_model
+import re
 
 User = get_user_model()
 
+def to_snake_case(text):
+    # Заменя интервали, скоби, запетаи и други с "_"
+    text = re.sub(r'\W+', '_', text)
+    return text.lower()
 
 class ExperimentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experiment
         fields = '__all__'
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        return {to_snake_case(key): value for key, value in data.items()}
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
